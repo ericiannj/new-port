dac33abd57d6f9e9e773be580c8e3f948984cff4
ericiannj <ericiannj@gmail.com>
2024-12-11 20:22:11 -0300

    feat(About): implement recommendations basic structure

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,6 +6,7 @@ import AnimatedSentences from './components/AnimatedSentences';
 import ProfileImage from '../../assets/images/profile.jpeg';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
+import { Icons } from '@/icons';
 
 type Direction = 'next' | 'prev';
 
@@ -14,6 +15,7 @@ export default function About() {
   const components = [
     { id: 'summary', Component: Summary },
     { id: 'perspectives', Component: Perspectives },
+    { id: 'recommendations', Component: Recommendations },
     // Add more components here as needed
   ];
 
@@ -109,7 +111,7 @@ export default function About() {
 }
 
 const Summary = () => (
-  <div className="flex h-full w-full flex-col gap-2 bg-orange-300">
+  <div className="flex h-full w-full flex-col gap-2">
     <p className="text-2xl">
       - Full Stack Developer with strong{' '}
       <span className="bg-gradient-to-r from-blue-500 via-purple-500 to-red-500 bg-clip-text text-transparent">
@@ -151,7 +153,7 @@ const Summary = () => (
 );
 
 const Perspectives = () => (
-  <div className="flex h-[215px] w-full flex-col gap-8 bg-blue-400">
+  <div className="flex h-[215px] w-full flex-col gap-8">
     <p className="text-2xl">
       I am committed to continually expanding skills, mainly with my current
       main stack (React JS/Next.js/Typescript) + knowledge of User Experience,
@@ -162,3 +164,103 @@ const Perspectives = () => (
     </p>
   </div>
 );
+
+const Recommendation1 = () => (
+  <div className="flex h-[215px] w-full flex-col gap-2">
+    <p className="text-base">
+      &quot;I had the great opportunity to work with Eric at Grupo Portfolio,
+      and it was incredible to see his progress in software development
+      projects. In terms of both technical skills and behavior, Eric has always
+      been a committed and studious professional who always strives for high
+      quality in everything he does (in all his deliveries).
+    </p>
+    <p className="text-base">
+      At the time, a notable case was a complex project we carried out, which
+      aimed to build a new architecture and restructure an application to enable
+      the implementation of new methods, services, libraries, and components
+      (including updating frameworks). His work was fundamental to the success
+      of the project, which he was able to apply and develop in Front-end,
+      Back-end, and DevOps concepts.&quot;
+    </p>
+    <p className="text-base">Thiago S.</p>
+  </div>
+);
+
+const Recommendation2 = () => (
+  <div className="flex h-[215px] w-full flex-col gap-2">
+    <p className="text-base">
+      &quot;Eric was a developer on my squad at Big Brain and from the beginning
+      he demonstrated excellent communication, concern for the product and the
+      quality of his work.
+    </p>
+    <p className="text-base">
+      Eric was a developer on my squad at Big Brain and from the beginning he
+      demonstrated excellent communication, concern for the product and the
+      quality of his work.&quot;
+    </p>
+    <p className="text-base">Heloísa G.</p>
+  </div>
+);
+
+const Recommendation3 = () => (
+  <div className="flex h-[215px] w-full flex-col gap-2">
+    <p className="text-base">
+      &quot;Eric was one of my first colleagues when I started working in
+      software development. What caught my attention the most about his way of
+      working and planning was his search for the best technologies and
+      methodologies on the market and his focus on creating scalable
+      applications that could always evolve.
+    </p>
+    <p className="text-base">
+      During the time I worked with him, I learned a lot and we evolved as
+      professionals, pushing each other to achieve excellence in projects. He is
+      a highly skilled professional. Few people have the courage to create an
+      application from scratch at the beginning of their career, but he accepted
+      this challenge together with our team without hesitation.&quot;
+    </p>
+    <p className="text-base">Gabriel A.</p>
+  </div>
+);
+
+const Recommendations = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const comments = [
+    { id: 0, component: Recommendation1 },
+    { id: 1, component: Recommendation2 },
+    { id: 2, component: Recommendation3 },
+  ];
+  const CurrentComponent = comments[selectedIndex].component;
+
+  return (
+    <div className="flex h-[215px] w-full flex-col gap-2">
+      <div className="flex flex-col items-center">
+        <CurrentComponent />
+        <div className="flex gap-4">
+          {Array.from({ length: comments.length }, (_, index) => (
+            <motion.button
+              key={index}
+              className={`rounded p-1 ${
+                selectedIndex === index ? 'bg-blue-300' : 'bg-transparent'
+              }`}
+              onClick={() => setSelectedIndex(index)}
+              animate={
+                selectedIndex === index
+                  ? {
+                      opacity: [1, 0.5, 1],
+                    }
+                  : {}
+              }
+              transition={
+                selectedIndex === index
+                  ? { repeat: Infinity, duration: 1.5, ease: 'easeInOut' }
+                  : {}
+              }
+            >
+              <Icons.about />
+            </motion.button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
