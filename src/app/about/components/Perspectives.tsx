8d93fc35b0d31964d5f2fa236c1b0f4db5e1e5af
ericiannj <ericiannj@gmail.com>
2025-01-27 21:01:10 -0300

    feat(Perspectives): implement gif points

--- src/app/about/components/Perspectives.tsx
+++ src/app/about/components/Perspectives.tsx
@@ -1,23 +1,51 @@
-import { Icons } from '@/icons';
+import TargetGif from '../../../assets/gifs/target.gif';
+import ResumeGif from '../../../assets/gifs/resume.gif';
+import Image from 'next/image';
 
 const Perspectives = () => (
   <div className="flex w-full flex-col gap-8">
-    <div className="text-fluid-2xl">
-      <p className="indent-8">
-        Currently, I&apos;m primarily focused on front-end development, with
-        over 70% of my work in this area.
-      </p>
-      <p className="indent-8">
-        Alongside this, I&apos;m enhancing my back-end skills, especially with
-        JavaScript technologies like Next.js and Express, and exploring the Go
-        programming language.
-      </p>
-      <p className="indent-8">
-        This helps me stay versatile and meet the changing needs of the tech
-        industry.
-      </p>
+    <div className="flex flex-col space-y-6 text-fluid-2xl">
+      <div className="flex items-center gap-4">
+        <Image
+          alt="target animation"
+          width={32}
+          height={32}
+          src={TargetGif}
+          className="rounded-full"
+        />
+        <p>
+          Currently, I&apos;m primarily focused on front-end development, with
+          over 70% of my work in this area.
+        </p>
+      </div>
+      <div className="flex items-center gap-4">
+        <Image
+          alt="target animation"
+          width={32}
+          height={32}
+          src={TargetGif}
+          className="rounded-full"
+        />
+        <p>
+          Alongside this, I&apos;m enhancing my back-end skills, especially with
+          JavaScript technologies like Next.js and Express, and exploring the Go
+          programming language.
+        </p>
+      </div>
+      <div className="flex items-center gap-4">
+        <Image
+          alt="target animation"
+          width={32}
+          height={32}
+          src={TargetGif}
+          className="rounded-full"
+        />
+        <p>
+          This helps me stay versatile and meet the changing needs of the tech
+          industry.
+        </p>
+      </div>
     </div>
-
     <div className="flex justify-center">
       <a
         href="/resume.pdf"
@@ -25,7 +53,13 @@ const Perspectives = () => (
         rel="noopener noreferrer"
         className="flex transform items-center gap-2 rounded-lg p-3 text-slate-300 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-gray-100 hover:text-gray-900"
       >
-        <Icons.resume className="h-6 w-6" />
+        <Image
+          alt="resume animation"
+          width={32}
+          height={32}
+          src={ResumeGif}
+          className="rounded-full"
+        />
         <span className="text-lg font-medium">My Resume</span>
       </a>
     </div>
