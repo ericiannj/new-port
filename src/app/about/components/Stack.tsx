d438d7af435dbdf588896d022be2c53a60d43e85
ericiannj <ericiannj@gmail.com>
2026-04-10 09:58:50 -0300

    fix: add missing 'use client' directives

--- src/app/about/components/Stack.tsx
+++ src/app/about/components/Stack.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { tools } from '@/assets/icons/Tools';
--- src/app/about/components/Summary.tsx
+++ src/app/about/components/Summary.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import DomainsSection from './DomainsSection';
 
 import CustomProfileImage from './CustomProfileImage';
